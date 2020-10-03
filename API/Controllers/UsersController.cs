b0f557f47e997954799acbd7cb29300119683525
Carlos Encine <contato@carlosencine.com>
2020-10-03 00:49:59 -0300

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateNewUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetUserById), new {Id = user.Id}, user);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<User>> UpdateUser(int id, User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            var user = _context.Users.FindAsync(id);
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Application/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Application
+{
+    public class Class1
+    {
+        private int categoria = 8;
+
+        public string GetCategory()
+        {
+            string category = "";
+            switch (categoria)
+            {
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    category = "Junior";
+                    break;
+
+            }
+
+            return category;
+        }
+    }
+}
